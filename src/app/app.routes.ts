3f9ae7e10c56ae62ec817194eb5e89b57f526b74
Beso Bakuradze <official.beso.1@gmail.com>
2025-12-22 16:46:44 +0400

    Routing issues fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { Login } from './components/login/login';
 import { NotFound } from './components/not-found/not-found';
 import { authGuard } from './guards/auth-guard';
 
+
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'product/:id', component: ProductDetails },
